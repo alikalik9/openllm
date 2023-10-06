432c71244cf98fe15ea9e7989c36dfa613826727
Alek Raklin <a.rakhlin@web.de>
2023-10-06 18:12:50 +0200

    init

--- main.py
+++ main.py
@@ -1,54 +1,69 @@
-#!/usr/bin/env python3
 from typing import List, Tuple
-
 from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationBufferMemory
-
-
 from nicegui import Client, ui
 
-OPENAI_API_KEY = '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'  # TODO: set your OpenAI API key here
+OPENAI_API_KEY = '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'
 
-memory = ConversationBufferMemory
-llm = ConversationChain(llm=ChatOpenAI(model_name="codellama-34b-instruct", openai_api_key='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', openai_api_base="https://api.perplexity.ai"),memory=ConversationBufferMemory())
-messages: List[Tuple[str, str]] = []
-thinking: bool = False
+class ChatApp:
+    def __init__(self):
+        self.llm = ConversationChain(llm=ChatOpenAI(model_name="mistral-7b-instruct", openai_api_key='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', openai_api_base="https://api.perplexity.ai"), memory=ConversationBufferMemory())
+        self.messages = []
+        self.thinking = False
+
+    def on_value_change(self, ename="mistral-7b-instruct", etemp="1"):
+        self.llm = ConversationChain(llm=ChatOpenAI(model_name=ename, temperature=etemp, openai_api_key='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', openai_api_base="https://api.perplexity.ai"))
+
+    @ui.refreshable
+    async def chat_messages(self) -> None:
+        for name, text in self.messages:
+            ui.chat_message(text=text, name=name, sent=name == 'You').props(f"bg-color={('teal' if name == 'You' else 'primary')} text-color=white").classes("rounded-lg")
+        if self.thinking:
+            ui.spinner("dots",size='3rem')
+        await ui.run_javascript('window.scrollTo(0, document.body.scrollHeight)', respond=False)
+
+    async def send(self, text) -> None:
+        message = text.value
+        self.messages.append(('You', text.value))
+        self.thinking = True
+        text.value = ''
+        self.chat_messages.refresh()
+        response = await self.llm.arun(message)
+        self.messages.append(('GPT', response))
+        self.thinking = False
+        self.chat_messages.refresh()
+    
+    async def clear(self):
+        self.messages.clear()
+        self.chat_messages.refresh()
+        self.llm.memory.clear()
 
 
-@ui.refreshable
-async def chat_messages() -> None:
-    for name, text in messages:
-        ui.chat_message(text=text, name=name, sent=name == 'You')
-    if thinking:
-        ui.spinner(size='3rem').classes('self-center')
-    await ui.run_javascript('window.scrollTo(0, document.body.scrollHeight)', respond=False)
-
+chat_app = ChatApp()
 
 @ui.page('/')
 async def main(client: Client):
     async def send() -> None:
-        global thinking
-        message = text.value
-        messages.append(('You', text.value))
-        thinking = True
-        text.value = ''
-        chat_messages.refresh()
-        response = await llm.arun(message)
-        print(vars(llm))
-        messages.append(('Bot', response))
-        thinking = False
-        chat_messages.refresh()
+        await chat_app.send(text)
 
-    llm.memory.memory_variables
-
-    anchor_style = r'a:link, a:visited {color: inherit !important; text-decoration: none; font-weight: 500}'
-    ui.add_head_html(f'<style>{anchor_style}</style>')
     await client.connected()
 
-    with ui.column().classes('w-full max-w-2xl mx-auto items-stretch'):
-        select1 = ui.select(["llama-2-70b-chat", "llama-2-13b-chat", "codellama-34b-instruct","mistral-7b-instruct"], value="mistral-7b-instruct", on_change=lambda e: on_value_change(e.value))
-        await chat_messages()
+    with ui.header(fixed=False).classes('items-center p-0 px-1 h-[6vh] no-wrap gap-0').style('box-shadow: 0 2px 4px').classes('bg-slate-100'):
+        ui.button(on_click=lambda: drawer.toggle(), icon='menu').props('flat color=black')
+        ui.label('Chat to LLM 💬').on("click", lambda: ui.open("/")).classes("cursor-pointer w-full text-black text-base font-semibold md:text-[2rem]")
+    
+    with ui.left_drawer(bottom_corner=True).style('background-color: #d7e3f4') as drawer:
+        ui.button(icon="add", on_click=chat_app.clear).props("rounded")
+        ui.label("Model").classes("pt-5")
+        select = ui.select(["llama-2-70b-chat", "llama-2-13b-chat", "codellama-34b-instruct", "mistral-7b-instruct"], value="mistral-7b-instruct", on_change=lambda e: chat_app.on_value_change(ename=e.value)).classes("bg-slate-200")
+        ui.label("Temperature").classes("pt-5")
+        slider = ui.slider(min=0, max=2, step=0.1, value=5,on_change=lambda e: chat_app.on_value_change(etemp=e.value)).props("label-always")
+        ui.separator().classes("bg-black")
+
+    with ui.column().classes('w-full items-stretch'):
+        await chat_app.chat_messages()
+
 
     with ui.footer().classes('bg-white'), ui.column().classes('w-full max-w-3xl mx-auto my-6'):
         with ui.row().classes('w-full no-wrap items-center'):
@@ -58,5 +73,7 @@ async def main(client: Client):
                 .classes('w-full self-center').on('keydown.enter', send)
         ui.markdown('simple chat app built with [NiceGUI](https://nicegui.io)') \
             .classes('text-xs self-end mr-8 m-[-1em] text-primary')
-  
-ui.run(title='Chat with GPT-3 (example)') 
+
+
+
+ui.run(title='Chat with GPT-3 (example)')
---
+++ othermain.py
@@ -0,0 +1,73 @@
+from typing import List, Tuple, Union
+
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+
+from nicegui import Client, ui
+
+llm: Union[ConversationChain, None] = None 
+
+messages: List[Tuple[str, str, str]] = []
+thinking: bool = False
+
+
+@ui.refreshable
+async def chat_messages() -> None:
+    for name, text in messages:
+        ui.chat_message(text=text, name=name, sent=name == 'You')
+    if thinking:
+        ui.spinner(size='3rem').classes('self-center')
+    await ui.run_javascript('window.scrollTo(0, document.body.scrollHeight)', respond=False)
+
+
+@ui.page('/')
+async def main(client: Client):
+    async def send() -> None:
+        global thinking
+        message = text.value
+        messages.append(('You', text.value))
+        thinking = True
+        text.value = ''
+        chat_messages.refresh()
+        await query(message)
+        thinking = False
+
+    async def query(message: str) -> None:
+        global llm
+        if llm is None:
+            llm = ConversationChain(llm=ChatOpenAI(model_name="mistral-7b-instruct", openai_api_key='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', openai_api_base="https://api.perplexity.ai"))
+            
+        response = await llm.arun(message)
+        messages.append(('Bot', response))
+        chat_messages.refresh()
+
+    anchor_style = r'a:link, a:visited {color: inherit !important; text-decoration: none; font-weight: 500}'
+    ui.add_head_html(f'<style>{anchor_style}</style>')
+    await client.connected()
+
+    with ui.column().classes('w-full max-w-2xl mx-auto items-stretch'):
+        await chat_messages()
+
+    with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
+        ui.label('Chat with your AI').classes('text-2xl')
+
+    with ui.left_drawer(bottom_corner=True).style('background-color: #d7e3f4'):
+        ui.label('OpenAI private key').classes('text-lg mt-2')
+        openai_key = ui.input(label='Key', placeholder='Provide your key here')
+
+        ui.label('Exploration rate').classes('text-lg mt-2')
+        exploration_slider = ui.slider(min=0, max=1, value=0.5, step=0.1)
+        ui.label().bind_text_from(exploration_slider, 'value')
+
+        ui.label('Maximum length of response').classes('text-lg mt-2')
+        token_slider = ui.slider(min=100, max=4000, value=2000, step=10)
+        ui.label().bind_text_from(token_slider, 'value')
+
+    with ui.footer().classes('bg-white'), ui.column().classes('w-full max-w-3xl mx-auto my-6'):
+        with ui.row().classes('w-full no-wrap items-center'):
+            text = ui.input(placeholder='message').props('rounded outlined input-class=mx-3') \
+                .classes('w-full self-center').on('keydown.enter', send)
+        ui.markdown('simple chat app built with [NiceGUI](https://nicegui.io)') \
+            .classes('text-xs self-end mr-8 m-[-1em] text-primary')
+
+ui.run(title='Chat with GPT-3 (example)')
