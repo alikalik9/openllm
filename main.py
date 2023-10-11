2e3bab54ff9e3d3fefbd74fab5b9886c3b4fe9c6
Alek Raklin <a.rakhlin@web.de>
2023-10-11 07:41:54 +0200

    changes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ API_KEY = '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'
 
 class ChatApp:
     def __init__(self):
-        self.llm = ConversationChain(llm=ChatOpenAI(model_name="mistral-7b-instruct", openai_api_key='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', openai_api_base="https://api.perplexity.ai"), memory=ConversationBufferMemory())
+        self.llm = ConversationChain(llm=ChatOpenAI(model_name="mistral-7b-instruct", openai_api_key='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', openai_api_base="https://api.perplexity.ai", temperature="0.1"), memory=ConversationBufferMemory())
         self.messages = []
         self.thinking = False
         self.total_tokens = 0
@@ -22,10 +22,20 @@ class ChatApp:
         self.current_chat_name = ""
 
     def on_value_change(self, ename="mistral-7b-instruct", etemp="1"):
+        """
+        Changes the value of the model and temperature for the ConversationChain.
+
+        Parameters:
+        ename (str): The name of the model.
+        etemp (str): The temperature for the model.
+        """
         self.llm = ConversationChain(llm=ChatOpenAI(model_name=ename, temperature=etemp, openai_api_key='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', openai_api_base="https://api.perplexity.ai"))
 
     @ui.refreshable
     async def chat_messages(self) -> None:
+        """
+        Displays the chat messages in the UI. Looks for the messages in the self.messages dict
+        """
         for name, text in self.messages:
             ui.chat_message(text=text, name=name, sent=name == 'You').props(f"bg-color={('teal' if name == 'You' else 'primary')} text-color=white").classes("rounded-lg text-lg")
         if self.thinking:
@@ -34,6 +44,10 @@ class ChatApp:
     
     @ui.refreshable
     def chat_history_grid(self): #function for the table with the chat history
+        """
+        Creates a grid for the chat history. Uses the aggrid. When clicking on a chat the load_chat_history function is invoked to load chat from json
+        """
+
         current_directory = os.getcwd()
         json_directory = os.path.join(current_directory, 'chat_history')   
         json_filenames = [f for f in os.listdir(json_directory) if f.endswith('.json')] #list all json files in directory
@@ -47,8 +61,14 @@ class ChatApp:
     'rowSelection': 'multiple',
 }, theme="material").classes('md:h-1/2 max-h-200 pt-4').on("cellClicked", lambda event:  self.load_chat_history(event.args["value"]))
 
-
     async def send(self, text) -> None:
+        """
+        Sends a message to the chat. Appends the self.messages list with the new message, sends it to the llm using the self.llm.arun function
+        also afte every sending the current chat is beeing updated in the json
+
+        Parameters:
+        text (str): The message to be sent. Text beeing given from the ui.input
+        """
         message = text.value
         self.messages.append(('You', text.value))
         self.thinking = True
@@ -59,20 +79,33 @@ class ChatApp:
         self.tokens_used = cb.total_tokens  # get the total tokens used
         print(self.tokens_used)
         self.messages.append(('GPT', response))
-        await self.save_to_db(self.messages_to_dict(self.llm.memory.chat_memory.messages))# Save the chat history to the database
+        await self.save_to_db(self.messages_to_dict(self.llm.memory.chat_memory.messages))# Update the chat history to the database
         self.chat_history_grid.refresh()
         self.thinking = False
         self.chat_messages.refresh()
 
     async def clear(self):
+        """
+        Clears the chat memory and messages to "open" a new chat
+        """
         self.llm.memory.clear()
         self.messages.clear()
         self.current_chat_name=""
+        self.tokens_used = "0"
         self.chat_messages.refresh()
         
 
 
     def messages_to_dict(self, messages: List) -> List[Dict]:
+        """
+        Helper function for saving the messages to json. Converts the chat messages to a dictionary.
+
+        Parameters:
+        messages (List): The list of messages.
+
+        Returns:
+        List[Dict]: The dictionary representation of the messages.
+        """
         return [{'type': type(m).__name__, 'content': m.content} for m in messages]
 
     async def save_to_db(self, data: List[Dict]) -> None:
@@ -117,6 +150,7 @@ class ChatApp:
         with get_openai_callback() as cb:
             response = await self.llm.arun("hi")
             print(response)
+        self.tokens_used = cb.total_tokens
         self.messages = [('You', message.content) if isinstance(message, HumanMessage) else ('GPT', message.content) for message in retrieved_messages]
         #self.chatloaded = True
         self.thinking = False
@@ -159,13 +193,15 @@ async def main(client: Client):
         ui.separator().classes("bg-black")
         ui.label("Tokens Used").classes("pt-3")
         ui.label("0").bind_text_from(chat_app,"tokens_used").classes("pt-1")
+        ui.separator().classes("bg-black")
+        ui.label("Current Chat").classes("pt-4")
+        with ui.row().classes("items-center justify-center bg-slate-100 no-wrap"):
+            ui.label("").bind_text_from(chat_app,"current_chat_name")
+            ui.button("", icon="delete", on_click=lambda e: ui.notify("hi")).bind_visibility_from(chat_app,"current_chat_name").props("rounded").classes("bg-red")
         chat_app.chat_history_grid()
         
 
     with ui.column().classes('w-full items-stretch'):
-        with ui.row().classes("items-center justify-center bg-slate-100"):
-            ui.label("").bind_text_from(chat_app,"current_chat_name")
-            ui.icon("delete", size="40px").bind_visibility_from(chat_app,"current_chat_name").on("click",ui.notify("hi"))
         await chat_app.chat_messages()
 
 
@@ -173,11 +209,11 @@ async def main(client: Client):
         with ui.row().classes('w-full no-wrap items-center'):
             placeholder = 'message' if API_KEY != 'not-set' else \
                 'Please provide your OPENAI key in the Python script first!'
-            text = ui.input(placeholder=placeholder).props('rounded outlined input-class=mx-3') \
+            text = ui.textarea(placeholder=placeholder).props('rounded outlined input-class=mx-3') \
                 .classes('w-full self-center').on('keydown.enter', send)
         ui.markdown('simple chat app built with [NiceGUI](https://nicegui.io)') \
             .classes('text-xs self-end mr-8 m-[-1em] text-primary')
 
 
 
-ui.run(title='Chat with GPT-3 (example)')
+ui.run(title='Chat with GPT-3 (example)', on_air="A2DCqSNCZ6Zbxv00")
