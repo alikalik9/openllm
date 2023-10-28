1968b86d9919fb5aa7c7b10956b3700acf86017d
Alek Raklin <a.rakhlin@web.de>
2023-10-28 12:19:12 +0200

    embeddings

--- embeddings.py
+++ embeddings.py
@@ -1,21 +1,38 @@
-import json
-import os
-from datetime import datetime
-from typing import List, Dict
-from langchain.chains import ConversationChain
-from langchain.chat_models import ChatOpenAI
-from langchain.callbacks import get_openai_callback
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain.schema import HumanMessage, AIMessage
-from langchain.memory.chat_memory import ChatMessageHistory
-from nicegui import Client, ui
-from main import ChatApp
+from llama_index import (
+    GPTVectorStoreIndex,
+    SimpleDirectoryReader,
+    download_loader,
+    LLMPredictor,
+    ServiceContext,
+    MockLLMPredictor,
+    load_index_from_storage
+    )
+from llama_index.storage.storage_context import StorageContext
+from chat import ChatApp
 
 class Embedding(ChatApp):
     def __init__(self):
         super().__init__()  # Call the initializer of the parent class
         # Add any additional initialization code here
+        self.llm_predictor = LLMPredictor(
+            llm=self.llm
+        )
+        self.service_context = ServiceContext.from_defaults(
+            llm_predictor=self.llm_predictor
+        )
 
     # Add any additional methods here
-    def print(self):
-        print(self.api_key)
+    def create_index(self, index_path):
+        self.documents = SimpleDirectoryReader("embedding_files", recursive=True).load_data()
+        self.index = GPTVectorStoreIndex.from_documents(
+            self.documents, service_context=self.service_context
+        )
+        self.index.storage_context.persist(persist_dir="index_path")
+       
+    async def load_index(self, indexpath):
+        storagecontext = StorageContext.from_defaults(persist_dir="indexpath")
+        self.index = load_index_from_storage(storagecontext)
+        return self.index
+    def chat(self, index,prompt):
+        query_engine = index.as_query_engine()
+        self.response = query_engine.query(prompt)
